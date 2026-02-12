d49682ba4d49e99610b2d69743524144691dfbc5
김영준 <dudwns1ms1234@gmail.com>
2026-02-12 15:08:42 +0900

    start

---
+++ app/Mynavbar/page.js
@@ -0,0 +1,20 @@
+'use client';
+
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+export default function MyNavbar() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="list">앨범목록</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
---
+++ app/list/page.js
@@ -0,0 +1,7 @@
+export default function List() {
+    return (
+        <div>
+            <h4>상품목록</h4>
+        </div>
+    )
+}
